416fa18ec2981cadefb64b3d373971532daae8bd
Dheeraj K Reddy <kdr.code@gmail.com>
2015-02-28 03:18:17 -0500

    Add exmaple 2-1
    Example 2-1: Simple demo to open an image and display it.

---
+++ chapter1/ex2-1/ex2-1.cpp
@@ -0,0 +1,24 @@
+/*
+Open and display an image form file.
+*/
+
+#include "highgui.h"
+#include <cstdio>
+
+int main( int argc, char *argv[] ){
+
+	if( argc != 2 ){
+		fprintf(stderr, "invalid arguments. ");
+		fprintf(stderr, "Usage:\n\t%s <image_path>\n", argv[0]);
+		exit(1);
+	}
+
+	IplImage* img = cvLoadImage( argv[1] );
+	cvNamedWindow( "Example 1", CV_WINDOW_AUTOSIZE );
+	cvShowImage( "Example 1", img );
+	cvWaitKey(0);
+	cvReleaseImage( &img );
+	cvDestroyWindow( "Example 1" );
+
+	exit(0);
+}
