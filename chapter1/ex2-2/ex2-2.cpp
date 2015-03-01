7b0c4a1398109bcff1eb88e87476aec464500b0f
Dheeraj K Reddy <kdr.code@gmail.com>
2015-03-01 00:49:13 -0500

    add stub for ex-2-2

---
+++ chapter1/ex2-2/ex2-2.cpp
@@ -0,0 +1,18 @@
+/*
+Play a video file from disk
+*/
+
+#include "highgui.h"
+#include <cstdio>
+
+int main( int argc, char *argv[] ){
+
+	if( argc != 2 ){
+		fprintf(stderr, "Error: invalid arguments.\n");
+		fprintf(stderr, "Usage:\n\t%s <image_path>\n", argv[0]);
+		exit(1);
+	}
+
+
+	exit(0);
+}
