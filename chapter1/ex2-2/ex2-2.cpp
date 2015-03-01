ac0d2da86b6e76641ee73613b50191d02fe8915d
Dheeraj K Reddy <kdr.code@gmail.com>
2015-03-01 02:04:34 -0500

    Complete program ex2-2.

--- chapter1/ex2-2/ex2-2.cpp
+++ chapter1/ex2-2/ex2-2.cpp
@@ -4,6 +4,8 @@ Play a video file from disk
 
 #include "highgui.h"
 #include <cstdio>
+#define WAIT_TIME 33 // in ms
+#define ESC_KEY 27 // ASCII value of ESC key
 
 int main( int argc, char *argv[] ){
 
@@ -13,6 +15,29 @@ int main( int argc, char *argv[] ){
 		exit(1);
 	}
 
+
+	CvCapture *capture = cvCreateFileCapture( argv[1] );
+	if( !capture ){
+		printf("error: could not open %s\n", argv[1]);
+		exit(1);
+	}
+
+	cvNamedWindow( "video", CV_WINDOW_AUTOSIZE );
+
+	while( true ){
+		static char c;
+
+		IplImage *frame = cvQueryFrame( capture );
+		if( !frame ) break;
+
+		cvShowImage( "video", frame );
+
+		c = cvWaitKey(WAIT_TIME);
+		if( c == ESC_KEY ) break;
+	}
+
+	cvReleaseCapture( &capture );
+	cvDestroyWindow( "video" );
 
 	exit(0);
 }
