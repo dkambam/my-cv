2fb632812d576ce85b28014885204571651efb10
Dheeraj K Reddy <kdr.code@gmail.com>
2015-03-10 04:00:40 -0400

    Extract exitIfInvalidArgs() method

--- chapter1/ex2-1/ex2-1.cpp
+++ chapter1/ex2-1/ex2-1.cpp
@@ -4,14 +4,11 @@ Open and display an image from file.
 
 #include "highgui.h"
 #include <cstdio>
+void exitIfInvalidArgs(int argc, char*argv[]);
 
-int main( int argc, char *argv[] ){
+int main( int argc, char *argv[]){
 
-	if( argc != 2 ){
-		fprintf(stderr, "Error: invalid arguments.\n");
-		fprintf(stderr, "Usage:\n\t%s <image_path>\n", argv[0]);
-		exit(1);
-	}
+	exitIfInvalidArgs(argc, argv);	
 
 	IplImage* img = cvLoadImage( argv[1] );
 	cvNamedWindow( "Example 1", CV_WINDOW_AUTOSIZE );
@@ -21,4 +18,12 @@ int main( int argc, char *argv[] ){
 	cvDestroyWindow( "Example 1" );
 
 	exit(0);
+}
+
+void exitIfInvalidArgs(int argc, char*argv[]){
+	if( argc != 2 ){
+		fprintf(stderr, "Error: invalid arguments.\n");
+		fprintf(stderr, "Usage:\n\t%s <image_path>\n", argv[0]);
+		exit(1);
+	}
 }
